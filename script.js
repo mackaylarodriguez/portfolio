6c0b7218c8bb6c5ba128c5944f475310ecb7e8cc
mack <mackaylarodriguez6@gmail.com>
2026-04-06 18:52:12 -0700

    Fixing Spacing

--- script.js
+++ script.js
@@ -19,3 +19,36 @@ if (projectCarousel) {
     { passive: false }
   );
 }
+
+const slideShows = document.querySelectorAll("[data-slide-show]");
+
+slideShows.forEach((slideShow) => {
+  const slides = Array.from(slideShow.querySelectorAll(".project-slide"));
+  const previousButton = slideShow.querySelector(".slide-arrow-left");
+  const nextButton = slideShow.querySelector(".slide-arrow-right");
+
+  if (!slides.length || !previousButton || !nextButton) {
+    return;
+  }
+
+  let currentIndex = slides.findIndex((slide) => slide.classList.contains("is-active"));
+
+  if (currentIndex < 0) {
+    currentIndex = 0;
+    slides[0].classList.add("is-active");
+  }
+
+  const showSlide = (nextIndex) => {
+    slides[currentIndex].classList.remove("is-active");
+    currentIndex = (nextIndex + slides.length) % slides.length;
+    slides[currentIndex].classList.add("is-active");
+  };
+
+  previousButton.addEventListener("click", () => {
+    showSlide(currentIndex - 1);
+  });
+
+  nextButton.addEventListener("click", () => {
+    showSlide(currentIndex + 1);
+  });
+});
